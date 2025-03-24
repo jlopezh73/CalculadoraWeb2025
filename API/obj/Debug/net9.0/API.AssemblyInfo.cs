70bfe500b30f0d1941fb12c8243b615bced9edde
jlopezh73 <jlopezh73@gmail.com>
2025-03-24 16:54:54 -0600

    DockerCompose

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24a27c546a35a79969c8feff01cf080b409d4a6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1c0ea2eba8f8b13555126f3ec37410b09fe7b8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
